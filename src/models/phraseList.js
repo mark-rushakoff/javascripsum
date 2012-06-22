b8e3e1c3d14744c15b65d62fca530052db71481f
Mark Rushakoff <mark.rushakoff@gmail.com>
2012-06-22 13:09:14 -0700

    Add phrases and authors to phrase list

--- src/models/phraseList.js
+++ src/models/phraseList.js
@@ -1,5 +1,13 @@
 Javascripsum.Models.PhraseList = Backbone.Model.extend({
     url: function() {
         return "topics/" + this.id + ".json";
+    },
+
+    phrases: function() {
+        return this.get("phrases");
+    },
+
+    authors: function() {
+        return this.get("authors");
     }
 });
--- test/spec/models/phraseListSpec.js
+++ test/spec/models/phraseListSpec.js
@@ -5,9 +5,47 @@ describe("Javascripsum.Models.PhraseList", function() {
         model = new Javascripsum.Models.PhraseList({id: "fruit"});
     });
 
-    describe("url", function() {
-        it("is relative and contains the topic name", function() {
-            expect(model.url()).toBe("topics/fruit.json");
+    describe("fetching", function() {
+        beforeEach(function() {
+            model.fetch();
+        });
+
+        it("fetches from the right url", function() {
+            expect(mostRecentAjaxRequest().url).toBe("topics/fruit.json");
+        });
+
+        describe("once the fetch completes", function() {
+            beforeEach(function() {
+                var response = {
+                    phrases: [
+                        { phrase: "apple"},
+                        { phrase: "banana", definition: 'A slightly radioactive fruit', url: 'http://en.wikipedia.org/wiki/Banana'},
+                        { phrase: "cherry", definition: 'A fruit often found in slot machines' },
+                        { phrase: "durian", url: 'http://en.wikipedia.org/wiki/Durian' }
+                    ],
+                    authors: [
+                        { name: "Orange Pineapple", email: "orange@pineapple.com" },
+                        { name: "Lemon Mango" }
+                    ]
+                };
+                mostRecentAjaxRequest().response({status: 200, responseText: JSON.stringify(response)});
+            });
+
+            it("can return the phrases", function() {
+                expect(model.phrases()).toEqual([
+                    { phrase: "apple"},
+                    { phrase: "banana", definition: 'A slightly radioactive fruit', url: 'http://en.wikipedia.org/wiki/Banana'},
+                    { phrase: "cherry", definition: 'A fruit often found in slot machines' },
+                    { phrase: "durian", url: 'http://en.wikipedia.org/wiki/Durian' }
+                ]);
+            });
+
+            it("can return the authors", function() {
+                expect(model.authors()).toEqual([
+                    { name: "Orange Pineapple", email: "orange@pineapple.com" },
+                    { name: "Lemon Mango" }
+                ]);
+            });
         });
     });
 });
