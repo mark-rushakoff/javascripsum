bb1f1744bc5ade60ec71ff955a953794506f2f46
Mark Rushakoff <mark.rushakoff@gmail.com>
2012-06-28 13:04:00 -0700

    Flesh out GlossaryView#render

--- src/views/glossaryView.js
+++ src/views/glossaryView.js
@@ -1,8 +1,15 @@
 Javascripsum.Views.GlossaryView = Backbone.View.extend({
     render: function() {
-        var $list = $("<dd></dd>");
+        var $list = $("<dl></dl>");
         _.each(this.model.phrases(), function(phraseObj) {
-            $list.append($("<dl></dl>").text(phraseObj.phrase));
+            var $dt = $("<dt></dt>");
+            if (phraseObj.url) {
+                $dt.append($("<a></a>").attr("href", phraseObj.url).text(phraseObj.phrase));
+            } else {
+                $dt.text(phraseObj.phrase);
+            }
+            $list.append($dt);
+            $list.append($("<dd></dd>").text(phraseObj.definition));
         });
 
         this.$el.empty().append($list);
