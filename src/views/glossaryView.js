631ab4ee298d7b4b2a500c13dcc599f16333e015
Mark Rushakoff & Alexander Murmann <mark.rushakoff+ajmurmann@gmail.com>
2012-08-21 19:50:18 -0700

    Put glossary view in application view, don't show glossary entries without definition or URL

--- src/views/glossaryView.js
+++ src/views/glossaryView.js
@@ -1,7 +1,9 @@
 Javascripsum.Views.GlossaryView = Backbone.View.extend({
     render: function() {
         var $list = $("<dl></dl>");
-        _.each(this.model.phrases(), function(phraseObj) {
+        _.each(_.filter(this.model.phrases(), function(phraseObj){
+          return phraseObj.definition || phraseObj.url;
+        }), function(phraseObj) {
             var $dt = $("<dt></dt>");
             if (phraseObj.url) {
                 $dt.append($("<a></a>").attr("href", phraseObj.url).text(phraseObj.phrase));
