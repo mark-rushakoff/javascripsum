28ef62f22e730d110e26556117590c5a3ed1f3a1
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-01-25 00:34:43 -0800

    Extract randomInt to an anonymous function in Util.Generator

--- src/util/generator.js
+++ src/util/generator.js
@@ -1,33 +1,35 @@
 Javascripsum.Util.Generator = function(model) {
-    this.model = model;
+  this.model = model;
 };
 
-_.extend(Javascripsum.Util.Generator.prototype, {
+(function() {
+  function randomInt(lowerBound, exclusiveUpperBound) {
+    return Math.floor(Math.random() * (exclusiveUpperBound - lowerBound)) + lowerBound;
+  }
+
+  _.extend(Javascripsum.Util.Generator.prototype, {
     sentence: function(numPhrases) {
-        numPhrases || (numPhrases = this._randomInt(3, 7));
-        var phrases = _.pluck(this.model.phrases(), "phrase");
+      numPhrases || (numPhrases = randomInt(3, 7));
+      var phrases = _.pluck(this.model.phrases(), "phrase");
 
-        var sentence = _.map(_.range(numPhrases), function() {
-            return _.sample(phrases);
-        });
+      var sentence = _.map(_.range(numPhrases), function() {
+        return _.sample(phrases);
+      });
 
-        sentence[0] = _.string.capitalize(sentence[0]);
-        sentence[numPhrases - 1] = sentence[numPhrases - 1] + '.';
+      sentence[0] = _.string.capitalize(sentence[0]);
+      sentence[numPhrases - 1] = sentence[numPhrases - 1] + '.';
 
-        return sentence.join(' ');
+      return sentence.join(' ');
     },
 
     paragraph: function(numSentences) {
-        numSentences || (numSentences = this._randomInt(2, 8));
+      numSentences || (numSentences = randomInt(2, 8));
 
-        var paragraph = _.map(_.range(numSentences), function() {
-            return this.sentence();
-        }, this);
+      var paragraph = _.map(_.range(numSentences), function() {
+        return this.sentence();
+      }, this);
 
-        return paragraph.join(' ');
-    },
-
-    _randomInt: function(lowerBound, exclusiveUpperBound) {
-        return Math.floor(Math.random() * (exclusiveUpperBound - lowerBound)) + lowerBound;
+      return paragraph.join(' ');
     }
-});
+  });
+})();
